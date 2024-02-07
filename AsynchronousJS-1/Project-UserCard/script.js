74dc5bbeda6f1dc0a222932a7fbb026f1db5e2bf
shivanirathore24 <shivani.rathore2409@gmail.com>
2024-02-07 20:27:23 +0530

    UserCard: Populating the card

--- AsynchronousJS-1/Project-UserCard/script.js
+++ AsynchronousJS-1/Project-UserCard/script.js
@@ -1,28 +1,29 @@
 const divEle = document.querySelector(".card-container");
 
-/* intiate httprequest, req object will interact with server & retrieve data from url 
-  without having to do full page refresh
-  XMLHttpRequest - not much in use in modern time, legacy API
-  */
-const req = new XMLHttpRequest();
+function getDetails(id) {
+  const request = new XMLHttpRequest();
 
-req.open("GET", `https://dummyjson.com/users/1`);
-req.send(); //send request to server
+  request.open("GET", `https://dummyjson.com/users/${id}`);
+  request.send();
 
-//display something on screen on the screen, when request has been completed/data has been loaded
-req.addEventListener("load", function () {
-  // console.log(typeof req.responseText);  //string JSON
-  //console.log(JSON.parse(this.responseText));   //JSON.parse returns JS Object
+  request.addEventListener("load", function () {
+    //console.log(this.responseText);    //string JSON
+    //console.log(JSON.parse(this.responseText));  //JSON.parse returns JS Object
+    const data = JSON.parse(this.responseText);
+    console.log(data);
 
-  const data = JSON.parse(this.responseText);
-  console.log(data);
-  const card = `<div class="user-card">
-  <img src="https://robohash.org/hicveldicta.png" alt="Profile Image" />
-  <h3>Shivani</h3>
-  <h3>Rathore</h3>
-  <p class="email">shiv.vani@gmail.com</p>
-  <button class="btn">View Profile</button>
-  </div>`;
+    const card = `<div class="user-card">
+    <img src=${data.image} alt="Profile Image" />
+        <h3>${data.firstName}</h3>
+        <h3>${data.lastName}</h3>
+        <p class="email">${data.email}</p>
+        <button class="btn">View Profile</button>
+        </div>`;
 
-  divEle.innerHTML = card;
-});
+    divEle.insertAdjacentHTML("beforeend", card);
+  });
+}
+
+getDetails(2);
+getDetails(4);
+getDetails(1);
