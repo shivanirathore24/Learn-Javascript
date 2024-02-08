907dd9a28936f9979e019c67278026db77d39628
shivanirathore24 <shivani.rathore2409@gmail.com>
2024-02-08 19:50:22 +0530

    UserCard: Nested Callbacks

--- AsynchronousJS-1/Project-UserCard/script.js
+++ AsynchronousJS-1/Project-UserCard/script.js
@@ -11,19 +11,32 @@ function getDetails(id) {
     //console.log(JSON.parse(this.responseText));  //JSON.parse returns JS Object
     const data = JSON.parse(this.responseText);
     console.log(data);
+    displayUser(data, "beforeend");
 
-    const card = `<div class="user-card">
-    <img src=${data.image} alt="Profile Image" />
-        <h3>${data.firstName}</h3>
-        <h3>${data.lastName}</h3>
-        <p class="email">${data.email}</p>
-        <button class="btn">View Profile</button>
-        </div>`;
+    const request2 = new XMLHttpRequest();
 
-    divEle.insertAdjacentHTML("beforeend", card);
+    request2.open("GET", `https://dummyjson.com/users/${id - 1}`);
+    request2.send();
+
+    request2.addEventListener("load", function () {
+      const data = JSON.parse(this.responseText);
+      console.log(data);
+      displayUser(data, "afterbegin", "other");
+    });
   });
 }
 
+function displayUser(data, pos, className = "") {
+  const card = `<div class="user-card ${className}">
+  <img src=${data.image} alt="Profile Image" />
+      <h3>${data.firstName}</h3>
+      <h3>${data.lastName}</h3>
+      <p class="email">${data.email}</p>
+      <button class="btn">View Profile</button>
+      </div>`;
+
+  divEle.insertAdjacentHTML(pos, card);
+}
+
 getDetails(2);
-getDetails(4);
-getDetails(1);
+//used nested-callback: order of cards shouldn't change after refreshing page
